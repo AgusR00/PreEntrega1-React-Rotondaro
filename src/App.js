e1ab90efeb4fca2f613c0bbd3fe3a4644ff44801
coder64700 <arotondaro2000@gmail.com>
2025-03-16 23:24:37 -0300

    add-cart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import ItemCount from './components/ItemCount/ItemCount';
+// import ItemCount from './components/ItemCount/ItemCount';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
+import { CartProvider } from './context/CartContext';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ItemListContainer greeting="Bienvenidos a la tienda"/>} />
-          <Route path="/category/:categoryId" element={<ItemListContainer />} />
-          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<ItemListContainer greeting="Bienvenidos a la tienda" />} />
+            <Route path="/category/:categoryId" element={<ItemListContainer />} />
+            <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
