ea4ba107d096a3f418699748e37b3a91cf9a4fcc
coder64700 <arotondaro2000@gmail.com>
2025-03-22 18:04:45 -0300

    add-button

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path='/' element={<ItemListContainer />}/>
           <Route path="/productos/:category" element={<ItemListContainer />} />
           <Route path="/productos/" element={<ItemListContainer />} />
-          <Route path="/item/:id" element={<ItemDetailContainer />}/>
+          <Route path="/item/:id" element={<ItemDetailContainer />} /> {/*Busca la información de un item por su id*/}
         </Routes>
 
       </BrowserRouter>
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react"
+import ItemCount from "../ItemCount/ItemCount"
 import "./ItemDetail.css"
 
-const ItemDetail = ( {item} ) => {
+const ItemDetail = ({ item }) => {
+
+  const [cantidad, setCantidad] = useState(1)
+
+  const handleRestar = () => {
+    cantidad > 1 && setCantidad(cantidad - 1)
+  }
+
+  const handleSumar = () => {
+    cantidad < item.stock && setCantidad(cantidad + 1)
+  }
+
+  const handleAgregar = () => {
+    console.log(...item, cantidad);
+}
+
   return (
     <div className="container">
+      <div className="producto-detalle">
+        <img className="producto-imagen" src={item.img} alt={item.name} />
         <div className="producto-detalle">
-        <img src={item.img} alt={item.name} />
-        <div>
-            <h3 className="titulo">{item.name}</h3>
-            <p className="descripcion">{item.description}</p>
-            <p className="categoria">Categoria: {item.category}</p>
-            <p className="precio">{item.price}</p>
+          <h3 className="titulo">{item.name}</h3>
+          <p className="descripcion">{item.description}</p>
+          <p className="categoria">Categoria: {item.category}</p>
+          <p className="precio">${item.price}</p>
+          <ItemCount cantidad = {cantidad} handleSumar = {handleSumar} handleRestar = {handleRestar} handleAgregar = {handleAgregar} />
         </div>
-        </div>
+      </div>
     </div>
   )
 }
