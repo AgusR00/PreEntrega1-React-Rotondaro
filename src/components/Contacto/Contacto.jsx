df42f71f284861c6607ca36fc8af08722076c03f
coder64700 <arotondaro2000@gmail.com>
2025-03-23 18:47:54 -0300

    fix-contacto

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import './Contacto.css'
 
 const Contacto = () => {
-
-
     const [valores, setValores] = useState({
         nombre: "",
         email: "",
@@ -24,33 +22,34 @@ const Contacto = () => {
 
     return (
         <div className='container-form'>
+            <div className='form-box'>
+                <h1 className='main-title'>Contacto</h1>
 
-            <h1 className='main-title'>Contacto</h1>
-
-            <form className='formulario' onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder='Ingresá tu nombre'
-                    value={valores.nombre}
-                    onChange={handleValores}
-                    name='nombre'
-                />
-                <input
-                    type="email"
-                    placeholder='Ingresá tu e-mail'
-                    value={valores.email}
-                    onChange={handleValores}
-                    name='email'
-                />
-                <input
-                    type="phone"
-                    placeholder='Ingresá tu telefono'
-                    value={valores.telefono}
-                    onChange={handleValores}
-                    name='telefono'
-                />
-                <button className='enviar' type='submit'>Enviar</button>
-            </form>
+                <form className='formulario' onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder='Ingresá tu nombre'
+                        value={valores.nombre}
+                        onChange={handleValores}
+                        name='nombre'
+                    />
+                    <input
+                        type="email"
+                        placeholder='Ingresá tu e-mail'
+                        value={valores.email}
+                        onChange={handleValores}
+                        name='email'
+                    />
+                    <input
+                        type="tel"
+                        placeholder='Ingresá tu telefono'
+                        value={valores.telefono}
+                        onChange={handleValores}
+                        name='telefono'
+                    />
+                    <button className='enviar' type='submit'>Enviar</button>
+                </form>
+            </div>
         </div>
     )
 }
