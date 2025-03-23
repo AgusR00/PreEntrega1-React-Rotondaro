10d7e4c061107c5470a585bfd67b318127083d6e
coder64700 <arotondaro2000@gmail.com>
2025-03-23 16:10:49 -0300

    fix-bugs

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,42 +1,44 @@
-import { useEffect, useState } from "react";
+"use client"
+
+import { useEffect, useState } from "react"
 import ItemList from "../ItemList/ItemList"
 import { useParams } from "react-router-dom"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "../../firebase/config"
 
 const ItemListContainer = () => {
+  const [productos, setProductos] = useState([])
+  const [titulo, setTitulo] = useState("Productos")
+  const category = useParams().category
 
-    const [productos, setProductos] = useState([])
+  useEffect(() => {
+   
+    if (category) {
 
-    const [titulo, setTitulo] = useState('Productos');
-
-    const category = useParams().category;
-
-    useEffect(() => {
-
-        const productosRef = collection(db, 'productos')
-
-        const q = category ? query(productosRef, where('category', '==', category)) : productosRef;
-
-        getDocs(q)
-            .then((resp) => {
-                
-                setProductos(
-                    resp.docs.map((doc) => {
-                        return { ...doc.data(), id: doc.id }
-                    })
-
-                )
-            })
-
-    }, [category])
-
-
-        return (
-            <div>
-                <ItemList productos={productos} titulo={titulo} />
-            </div>
-        )
+      const categoryTitle = category.charAt(0).toUpperCase() + category.slice(1)
+      setTitulo(categoryTitle)
+    } else {
+      setTitulo("Productos")
     }
 
+    const productosRef = collection(db, "productos")
+    const q = category ? query(productosRef, where("category", "==", category)) : productosRef
+
+    getDocs(q).then((resp) => {
+      setProductos(
+        resp.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id }
+        }),
+      )
+    })
+  }, [category])
+
+  return (
+    <div>
+      <ItemList productos={productos} titulo={titulo} />
+    </div>
+  )
+}
+
 export default ItemListContainer
+
